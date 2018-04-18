f620808d05bcf8896e83574e011ff1e562c5599e
Daniel Ngo <dn7665@truman.edu>
2018-04-18 18:09:27 -0500

    Removed uneeded comments

--- Project2/ProxyThread.java
+++ Project2/ProxyThread.java
@@ -6,111 +6,71 @@ import java.util.*;
 
 public class ProxyThread extends Thread
 {
-   private Socket           socket      = null;
+   private Socket socket = null;
    private static final int BUFFER_SIZE = 32768;
 
    public ProxyThread(Socket socket)
    {
-      super("ProxyThread");
       this.socket = socket;
    }
 
    public void run()
    {
-      // get input from user
-      // send request to server
-      // get response from server
-      // send response to user
-
-      System.out.println("This is the beginning of the thread.\n\n");
+      System.out.println("Thread start\n");
 
       try
       {
-         System.out.println("This <something> Connected\n");
          DataOutputStream out = new DataOutputStream(socket.getOutputStream());
          BufferedReader in = new BufferedReader(
-               new InputStreamReader(socket.getInputStream()));
+        		 new InputStreamReader(socket.getInputStream()));
 
-         String inputLine, outputLine;
-         int cnt = 0;
+         String inputLine;
+         int count = 0;
          String urlToCall = "";
 
-         /////////////////////////////////
-         // begin get request from client//
-         /////////////////////////////////
+         
+         // Get request from client
          while ((inputLine = in.readLine()) != null)
          {
             try
             {
-               System.out.println(inputLine);
-
-               StringTokenizer tok = new StringTokenizer(inputLine);
-               tok.nextToken();
+                System.out.println(inputLine);
+                StringTokenizer tok = new StringTokenizer(inputLine);
+                tok.nextToken();
             }
             catch (Exception e)
             {
                break;
             }
             // parse the first line of the request to find the url
-            if (cnt == 0)
+            if (count == 0)
             {
                String[] tokens = inputLine.split(" ");
                urlToCall = tokens[1];
-               // can redirect this to output log
-               System.out.println("Request for : " + urlToCall + "\n");
             }
 
-            cnt++;
+            count++;
          }
-         ////////////////////////////////
-         // end get request from client//
-         ////////////////////////////////
 
+         // Send client request to the server
          BufferedReader rd = null;
+         InputStream is = null;
          try
          {
-            System.out.println(
-                  "sending request " + "to real server for url: " + urlToCall + "\n");
-            
-            ///////////////////////////////////
-            // begin send request to server, get response from server
-            ///////////////////////////////////
             URL url = new URL(urlToCall);
             URLConnection conn = url.openConnection();
             conn.setDoInput(true);
-            
-            // not doing HTTP posts
             conn.setDoOutput(false);
-            System.out.println("Type is: " + conn.getContentType());
-            System.out.println("content length: " + conn.getContentLength());
-            System.out.println("allowed user interaction: "
-                  + conn.getAllowUserInteraction());
-            System.out
-                  .println("content encoding: " + conn.getContentEncoding());
-            System.out.println("content type: " + conn.getContentType());
 
-            // Get the response
-            InputStream is = null;
-            HttpURLConnection huc = (HttpURLConnection) conn;
+            //Don't know how this works
             if (conn.getContentLength() > 0)
             {
-               try
-               {
-                  is = conn.getInputStream();
-                  rd = new BufferedReader(new InputStreamReader(is));
-               }
-               catch (IOException ioe)
-               {
-                  System.out
-                        .println("********* IO EXCEPTION **********: " + ioe);
-               }
+              is = conn.getInputStream();
+              rd = new BufferedReader(new InputStreamReader(is));
             }
-            // end send request to server, get response from server
-            ///////////////////////////////////
 
-            //////////////////////////////////
-            // begin send response to client//
-            //////////////////////////////////
+            
+            // Send response to client
             byte by[] = new byte[BUFFER_SIZE];
             int index = is.read(by, 0, BUFFER_SIZE);
             
@@ -120,16 +80,10 @@ public class ProxyThread extends Thread
                index = is.read(by, 0, BUFFER_SIZE);
             }
             out.flush();
-            ////////////////////////////////
-            // end send response to client//
-            ////////////////////////////////
          }
          catch (Exception e)
          {
-            // can redirect this to error log
             System.err.println("Encountered exception: " + e);
-            // encountered error - just send nothing back, so
-            // processing can continue
             out.writeBytes("");
          }
 
