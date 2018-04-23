62e5eea1225105466812e6b442029a1c28fd2a58
Monica <ms4738@truman.edu>
2018-04-22 20:42:47 -0500

    Modifed proxyServer thread so that it refreshes the cache for modifications every 30 seconds. Cleaned up code

--- Project2/ProxyServer.java
+++ Project2/ProxyServer.java
@@ -1,6 +1,7 @@
 package Project2;
 
 import java.net.*;
+import java.text.SimpleDateFormat;
 import java.io.*;
 import java.util.Date;
 import java.util.concurrent.ConcurrentHashMap;
@@ -11,9 +12,9 @@ class WebsiteInfo
 {
 	private byte[] websiteBody;
 	private Date lastModified;
-	private int timeRetreived; //(int)System.currentTimeMillis()/1000;
+	private int timeRetreived; 
 	private int statusCode;
-	
+
 	public void setBody(byte[] bytes)
 	{
 		websiteBody = bytes;
@@ -46,9 +47,9 @@ class WebsiteInfo
 	{
 		return statusCode;
 	}
-	public void printWebInfo()
+	public void printWebInfo() throws UnsupportedEncodingException
 	{
-		System.out.println("Body: " + this.getBody().toString());
+		System.out.println("Body: " + new String(this.getBody(), "UTF-8"));
 		System.out.println("TimeRetreived: " + this.getTimeRetreived());
 		System.out.println("LastModifed: " + this.getLastModified());
 		System.out.println("StatusCode: " + this.getStatusCode() + "\n");
@@ -58,71 +59,113 @@ class WebsiteInfo
 public class ProxyServer extends Thread
 {
 	static ConcurrentMap<String, WebsiteInfo> cache;
-	
+
+	/**
+	 * Refreshes the cache every 30 seconds to make sure that 
+	 * recent modifications of the websites have been updated in the cache
+	 */
 	public void run()
-    {
-		int deleteThreshold = 30;
-    	while (true)
-    	{
-    		try {
-				TimeUnit.SECONDS.sleep(deleteThreshold + 1);
-			} catch (InterruptedException e) {
+	{
+		int refresh = 30;
+		while (true)
+		{
+			try 
+			{
+				TimeUnit.SECONDS.sleep(refresh + 1);
+			} 
+			catch (InterruptedException e) 
+			{
 				e.printStackTrace();
 			}
-    		
-    		//Delete unused websites
-    		cache.forEach((siteAddress, websiteInfo) ->
-    		{
-    			if (websiteInfo.getTimeRetreived() + deleteThreshold < (int)System.currentTimeMillis()/1000)
-    			{
-    				System.out.println("Site has been removed");
-    				cache.remove(siteAddress);
-    			}
-    		});
-//    		System.out.println("ALL CACHED WEBSITES");
-//    		cache.forEach((siteAddress, websiteInfo) ->
-//	        {
-//				System.out.println("SiteAddress: " + siteAddress);
-//				System.out.println("Body: " + websiteInfo.getBody().toString());
-//				System.out.println("TimeRetreived: " + websiteInfo.getTimeRetreived());
-//				System.out.println("LastModifed: " + websiteInfo.getLastModified());
-//				System.out.println("StatusCode: " + websiteInfo.getStatusCode() + "\n");
-//	        });
-    	}
-    }
-	
-   @SuppressWarnings("resource")
-public static void main(String[] args) throws IOException
-   {
-      ServerSocket serverSocket = null;
-      cache = new ConcurrentHashMap<>();
-      
-      ProxyServer server = new ProxyServer();
-      server.start();
-      
-      int port = 10000; // default
-      try
-      {
-         port = Integer.parseInt(args[0]);
-      }
-      catch (Exception e)
-      {
-      }
 
-      try
-      {
-         serverSocket = new ServerSocket(port);
-         System.out.println("Started on: " + port);
-      }
-      catch (IOException e)
-      {
-         System.err.println("Could not listen on port: " + args[0]);
-         System.exit(-1);
-      }
+			//Refresh cache for modified web urls
+			cache.forEach((siteAddress, websiteInfo) ->
+			{
+				InputStream serverToProxy = null;
+				try
+				{
+					SimpleDateFormat format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
+					URL connection = new URL(siteAddress);
+					HttpURLConnection conn = (HttpURLConnection) connection.openConnection();
+					String holdStr = conn.getHeaderField("Last-Modified");
+					Date d = format.parse(holdStr);
+					long cachedMillis = websiteInfo.getLastModified().getTime();
+					long websiteMillis = d.getTime();
+					//this happens if modified time is more recent
+					if(cachedMillis <= websiteMillis)
+					{
+						websiteInfo.setStatusCode(conn.getResponseCode());
+						websiteInfo.setTimeRetreived((int)System.currentTimeMillis()/1000);
+						websiteInfo.setLastModified(d);
+						if (conn.getContentLength() > 0)
+						{
+							serverToProxy = conn.getInputStream();
+						}
 
-      while (true)
-      {
-         new ProxyThread(serverSocket.accept()).start();
-      }
-   }
+
+						try
+						{
+
+							ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+							int nRead;
+							byte[] data = new byte[1024];
+							while ((nRead = serverToProxy.read(data, 0, data.length)) != -1) {
+								buffer.write(data, 0, nRead);
+							}
+							buffer.flush();
+							byte[] byteArray = buffer.toByteArray();
+							websiteInfo.setBody(byteArray);
+						}
+						catch (Exception e)
+						{
+							System.err.println("Encountered exception: " + e);
+						}
+						System.out.println("UPDATED:");
+						websiteInfo.printWebInfo();
+					}
+					
+				}
+				catch(Exception e)
+				{
+					e.printStackTrace();
+				}
+
+			});
+		}
+	}
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws IOException
+	{
+		ServerSocket serverSocket = null;
+		cache = new ConcurrentHashMap<>();
+
+		ProxyServer server = new ProxyServer();
+		server.start();
+
+		int port = 10000; // default
+		try
+		{
+			port = Integer.parseInt(args[0]);
+		}
+		catch (Exception e)
+		{
+		}
+
+		try
+		{
+			serverSocket = new ServerSocket(port);
+			System.out.println("Started on: " + port);
+		}
+		catch (IOException e)
+		{
+			System.err.println("Could not listen on port: " + args[0]);
+			System.exit(-1);
+		}
+
+		while (true)
+		{
+			new ProxyThread(serverSocket.accept()).start();
+		}
+	}
 }
--- Project2/ProxyThread.java
+++ Project2/ProxyThread.java
@@ -8,231 +8,206 @@ import java.util.*;
 
 public class ProxyThread extends Thread
 {
-   private Socket socket = null;
-   
-   public DataOutputStream proxyToClient;
-   public BufferedReader clientToProxy;
-   public InputStream serverToProxy;
-   public boolean isCached;
-   WebsiteInfo cachedWebsite;
-   
-   public ProxyThread(Socket socket)
-   {
-      this.socket = socket;
-      isCached = true;
-   }
+	private Socket socket = null;
 
-   public void run()
-   {
-      try
-      {
-         proxyToClient = new DataOutputStream(socket.getOutputStream());
-         clientToProxy = new BufferedReader(
-        		 new InputStreamReader(socket.getInputStream()));
-      System.out.println("Thread start\n");
-      String urlToCall = "";
+	public DataOutputStream proxyToClient;
+	public BufferedReader clientToProxy;
+	public InputStream serverToProxy;
+	public boolean isCached;
+	WebsiteInfo cachedWebsite;
 
-         String inputLine;
-         int count = 0;
+	public ProxyThread(Socket socket)
+	{
+		this.socket = socket;
+		isCached = true;
+	}
 
-         // Get request from client
-         while ((inputLine = clientToProxy.readLine()) != null)
-         {
-            try
-            {
-//               System.out.println(inputLine);
-               StringTokenizer tok = new StringTokenizer(inputLine);
-               tok.nextToken();
-            }
-            catch (Exception e)
-            {
-               break;
-            }
-            // parse the first line of the request to find the url
-            if (count == 0)
-            {
-               String[] tokens = inputLine.split(" ");
-               urlToCall = tokens[1];
-            }
+	public void run()
+	{
+		try
+		{
+			proxyToClient = new DataOutputStream(socket.getOutputStream());
+			clientToProxy = new BufferedReader(
+					new InputStreamReader(socket.getInputStream()));
+			System.out.println("Thread start\n");
+			String urlToCall = "";
+			String inputLine;
+			int count = 0;
 
-            count++;
-         }
-         
-         //Proxy -> Server, Server -> Proxy -> Client
-         cachedWebsite = ProxyServer.cache.get(urlToCall);
-         if (cachedWebsite == null)
-         {
-        	 System.out.println("This is a new request");
-        	 cachedWebsite = new WebsiteInfo();
-        	 isCached = false;
-        	 cacheServerString(urlToCall);
-         }
-         //TODO Check website for If-Modified-Since IF our predetermined threshhold is passed
-         //when client request from server. (THERE IS NO POLLING IN THE SERVER, CLIENT MUST ASK FIRST)
-         //If website has a different If-Modified-Since, then get that and update the Hashmap
-         else if (cachedWebsite.getTimeRetreived() + 10 < (int)System.currentTimeMillis()/1000)
-         {
-        	SimpleDateFormat format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
-       	   	URLConnection connection = new URL(urlToCall).openConnection();
-       	   	String holdStr = connection.getHeaderField("Last-Modified");
-       	   	Date d = format.parse(holdStr);
-       	   	long cachedMillis = cachedWebsite.getLastModified().getTime();
-       	   	long websiteMillis = d.getTime();
-       	   	//this happens if modified time is more recent
-       	   	if(cachedMillis != websiteMillis)
-       	   	{
-       	   		cacheServerString(urlToCall);
-       	   	}
-         }
-         else
-         {
-        	 cachedWebsite.setStatusCode(304);
-        	 ProxyServer.cache.put(urlToCall, cachedWebsite);
-         }
-    	 sendCachedString(cachedWebsite.getBody());
-         cachedWebsite = ProxyServer.cache.get(urlToCall);
-         System.out.println("URL:" + urlToCall);
-         cachedWebsite.printWebInfo();
-         
-         // Cleanup crew
-         if (proxyToClient != null)
-         {
-        	 proxyToClient.close();
-         }
-         if (clientToProxy != null)
-         {
-        	 clientToProxy.close();
-         }
-         if (serverToProxy != null)
-         {
-        	 serverToProxy.close();
-         }
-         if (socket != null)
-         {
-            socket.close();
-         }
+			// Get request from client
+			while ((inputLine = clientToProxy.readLine()) != null)
+			{
+				try
+				{
+					StringTokenizer tok = new StringTokenizer(inputLine);
+					tok.nextToken();
+				}
+				catch (Exception e)
+				{
+					break;
+				}
+				// parse the first line of the request to find the url
+				if (count == 0)
+				{
+					String[] tokens = inputLine.split(" ");
+					urlToCall = tokens[1];
+				}
+				count++;
+			}
 
-      }
-      catch (IOException | ParseException e)
-      {
-         e.printStackTrace();
-      }
-   }
-   
-   private void cacheServerString(String urlToCall)
-   {
-	   // Send client request to the server
-       try
-       {
-          URL url = new URL(urlToCall);
-          HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-          conn.setDoInput(true);
-          conn.setDoOutput(false);
-          
-        //Formats the date string into a Date
-          SimpleDateFormat format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
-          format.setTimeZone(TimeZone.getTimeZone("GMT-6"));
-          //We dont needs last modified on the date, just the date that we get it in
-          String holdStr = conn.getHeaderField("Last-Modified");
-          Date hold;
-          if (holdStr != null)
-          {
-        	  hold = format.parse(holdStr);
-        	  cachedWebsite.setLastModified(hold);
-//        	  System.out.println(new Date());
-          }
-          cachedWebsite.setStatusCode(conn.getResponseCode());
-          cachedWebsite.setTimeRetreived((int)System.currentTimeMillis()/1000);
-          
-          switch (conn.getResponseCode())
-          {
-          	case HttpURLConnection.HTTP_OK:
-          	//This gets the body of the request (i.e. the website)
-                if (conn.getContentLength() > 0)
-                {
-              	  serverToProxy = conn.getInputStream();
-                }
-          		saveResponse(urlToCall);
-          		break;
-          	case HttpURLConnection.HTTP_BAD_REQUEST:
-          		cachedWebsite.setBody(conn.getResponseMessage().getBytes());
-          		break;
-          	case HttpURLConnection.HTTP_NOT_IMPLEMENTED:
-          		cachedWebsite.setBody(conn.getResponseMessage().getBytes());
-          		break;
-          }
-        	  
-          
-          
-          //This outputs the http header information
-//          Map<String, List<String>> map = conn.getHeaderFields();
-//          for (Map.Entry<String, List<String>> entry : map.entrySet())
-//          {
-//        	  System.out.println("Key : " + entry.getKey()
-//        			  + " -> value : " + entry.getValue());
-//          }
-          
-          
-          
-          
-       }
-       catch (Exception e)
-       {
-    	   e.printStackTrace();
-       }
-       ProxyServer.cache.put(urlToCall, cachedWebsite);
-       
-       
-   }
-  
-public void saveResponse(String urlToCall)
-{
-	// Save response to hashmap
-    try
-    {
- 	   
- 	    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
- 	    int nRead;
- 	    byte[] data = new byte[1024];
- 	    while ((nRead = serverToProxy.read(data, 0, data.length)) != -1) {
- 	        buffer.write(data, 0, nRead);
- 	    }
- 	 
- 	    buffer.flush();
- 	    byte[] byteArray = buffer.toByteArray();
- 	
- 	    //assertThat(text, equalTo(originalString));
-       cachedWebsite.setBody(byteArray);
-//       System.out.println(byteArray.toString());
-       // -----> Actual String Representation   System.out.println(new String(byteArray, "UTF-8"));
-      
-       //ProxyServer.cacheDate.put(urlToCall, Date);
-    }
-    catch (Exception e)
-    {
-       System.err.println("Encountered exception: " + e);
-    }
-}
-   
-   //TODO Still need to also send 304, 400, 501 response codes to the client (in Header?)
-   private void sendCachedString(byte[] bytes)
-   {
-       // Send response to client
-       try
-       {
-          int index = bytes.length;
-          
-    	  proxyToClient.write(bytes, 0, index);
-          proxyToClient.flush();
+			//Proxy -> Server, Server -> Proxy -> Client
+			cachedWebsite = ProxyServer.cache.get(urlToCall);
+			if (cachedWebsite == null)
+			{
+				System.out.println("This is a new request");
+				cachedWebsite = new WebsiteInfo();
+				isCached = false;
+				cacheServerString(urlToCall);
+			}
+			else if (cachedWebsite.getTimeRetreived() + 10 < (int)System.currentTimeMillis()/1000)
+			{
+				SimpleDateFormat format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
+				URLConnection connection = new URL(urlToCall).openConnection();
+				String holdStr = connection.getHeaderField("Last-Modified");
+				Date d = format.parse(holdStr);
+				long cachedMillis = cachedWebsite.getLastModified().getTime();
+				long websiteMillis = d.getTime();
+				//this happens if modified time is more recent
+				if(cachedMillis != websiteMillis)
+				{
+					cacheServerString(urlToCall);
+				}
+			}
+			else
+			{
+				cachedWebsite.setStatusCode(304);
+				ProxyServer.cache.put(urlToCall, cachedWebsite);
+			}
+			sendCachedString(cachedWebsite.getBody());
+			cachedWebsite = ProxyServer.cache.get(urlToCall);
+			System.out.println("URL:" + urlToCall);
+			cachedWebsite.printWebInfo();
 
-//          This outputs the website onto console
-//          proxyToClient = new DataOutputStream(System.out);
-//          proxyToClient.write(bytes, 0, index);;
-//          proxyToClient.flush();
-       }
-       catch (Exception e)
-       {
-          e.printStackTrace();
-       }
-   }
+			// Cleanup crew
+			if (proxyToClient != null)
+			{
+				proxyToClient.close();
+			}
+			if (clientToProxy != null)
+			{
+				clientToProxy.close();
+			}
+			if (serverToProxy != null)
+			{
+				serverToProxy.close();
+			}
+			if (socket != null)
+			{
+				socket.close();
+			}
+
+		}
+		catch (IOException | ParseException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Method which adds an entry in the cache for the specific URL requested 
+	 * @param urlToCall
+	 */
+	private void cacheServerString(String urlToCall)
+	{
+		try
+		{
+			URL url = new URL(urlToCall);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoInput(true);
+			conn.setDoOutput(false);
+
+			//Formats the date string into a Date
+			SimpleDateFormat format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
+			format.setTimeZone(TimeZone.getTimeZone("GMT-6"));
+			String holdStr = conn.getHeaderField("Last-Modified");
+			Date hold;
+			if (holdStr != null)
+			{
+				hold = format.parse(holdStr);
+				cachedWebsite.setLastModified(hold);
+			}
+			cachedWebsite.setStatusCode(conn.getResponseCode());
+			cachedWebsite.setTimeRetreived((int)System.currentTimeMillis()/1000);
+
+			switch (conn.getResponseCode())
+			{
+			case HttpURLConnection.HTTP_OK:
+				//This gets the body of the request (i.e. the website)
+				if (conn.getContentLength() > 0)
+				{
+					serverToProxy = conn.getInputStream();
+				}
+				saveResponse(urlToCall);
+				break;
+			case HttpURLConnection.HTTP_BAD_REQUEST:
+				cachedWebsite.setBody(conn.getResponseMessage().getBytes());
+				break;
+			case HttpURLConnection.HTTP_NOT_IMPLEMENTED:
+				cachedWebsite.setBody(conn.getResponseMessage().getBytes());
+				break;
+			}
+
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		ProxyServer.cache.put(urlToCall, cachedWebsite);
+
+
+	}
+	
+	/**
+	 * Method which saves the response from the server to the cache
+	 * @param urlToCall
+	 */
+	public void saveResponse(String urlToCall)
+	{
+		try
+		{
+
+			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+			int nRead;
+			byte[] data = new byte[1024];
+			while ((nRead = serverToProxy.read(data, 0, data.length)) != -1) {
+				buffer.write(data, 0, nRead);
+			}
+
+			buffer.flush();
+			byte[] byteArray = buffer.toByteArray();
+			cachedWebsite.setBody(byteArray);
+		}
+		catch (Exception e)
+		{
+			System.err.println("Encountered exception: " + e);
+		}
+	}
+	
+	/**
+	 * Method which sends response from proxy to client
+	 * @param bytes
+	 */
+	private void sendCachedString(byte[] bytes)
+	{
+		try
+		{
+			int index = bytes.length;         
+			proxyToClient.write(bytes, 0, index);
+			proxyToClient.flush();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
